f7b9bb79f465c812543d723005f388e858314197
svalench <chitsalex@gmail.com>
2022-07-03 10:08:22 +0300

    refactor techonogies block

--- components/index/technologies.vue
+++ components/index/technologies.vue
@@ -1,26 +1,25 @@
 <template>
 <div>
 <b-container class="p-4">
-  <b-row>
-    <b-col>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-django-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-docker-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-html-5-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-javascript-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-kubernetes-96.svg')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-npm-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-postgresql-96.svg')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-vue-js-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-логотип-mysql-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-логотип-php-64.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-настройки-api-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-ос-линукс-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-питон-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-jira-96.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-jquery-50.png')" alt="Image 1"></b-img>
-      <b-img thumbnail fluid :src="require('~/assets/img/icon_tech/icons8-git-96.png')" alt="Image 1"></b-img>
+  <b-row style="padding: 3%">
+    <b-col style="color: #d4d9df">На какихя ЯП работаю:</b-col>
+    <b-col cols="8">
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/image 2.svg')" alt="Image 1"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/image 8.svg')" alt="Image 1"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/javascript 1.svg')" alt="Image 1"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/Python_lal 1.svg')" alt="Image 1"></b-img>
     </b-col>
-
+  </b-row>
+  <b-row style="padding: 3%">
+    <b-col style="color: #d4d9df">Фреймворки:</b-col>
+    <b-col  cols="8">
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/vue 1.svg')" alt="VueJs"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/image 23.svg')" alt="Knockout"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/image 25.svg')" alt="NodeJs"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/image 20.svg')" alt="Django"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/image 21.svg')" alt="FastApi"></b-img>
+      <b-img height="50px" style="background-color: rgba(26,49,49,0)" :src="require('~/assets/img/new_tech/image 22.svg')" alt="DRF"></b-img>
+    </b-col>
   </b-row>
 </b-container>
 </div>
@@ -28,7 +27,7 @@
 
 <script>
 export default {
-  name: "technologies"
+  name: "technologies",
 }
 </script>
 
